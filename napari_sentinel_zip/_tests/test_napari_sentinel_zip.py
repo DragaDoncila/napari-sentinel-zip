e2c3716421e4eee36dc7d25d5281f446326448e9
Draga Doncila Pop <ddon0001@student.monash.edu>
2020-05-20 19:22:23 +1000

    Adding tests and test data

--- napari_sentinel_zip/_tests/test_napari_sentinel_zip.py
+++ napari_sentinel_zip/_tests/test_napari_sentinel_zip.py
@@ -1,30 +1,79 @@
 import numpy as np
-from napari_napari_sentinel_zip import napari_get_reader
+from napari_sentinel_zip import napari_get_reader
+import os
+import napari.components
 
+TEST_TILE_PATH = os.path.abspath("./napari_sentinel_zip/_tests/Test_Tile")
 
-# tmp_path is a pytest fixture
-def test_reader(tmp_path):
-    """An example of how you might test your plugin."""
+IM_SHAPES = [
+    (5490, 5490),
+    (5490, 5490),
+    (10980, 10980),
+    (10980, 10980),
+    (10980, 10980),
+    (5490, 5490),
+    (5490, 5490),
+    (5490, 5490),
+    (10980, 10980),
+    (5490, 5490),
+    (5490, 5490),
+    (5490, 5490),
+    (10980, 10980),
+    (10980, 10980),
+    (10980, 10980),
+    (5490, 5490),
+    (5490, 5490),
+    (5490, 5490),
+    (10980, 10980),
+    (5490, 5490),
+    (1000, 1000, 3) #QKL
+]
 
-    # write some fake data using your supported file format
-    my_test_file = str(tmp_path / "myfile.npy")
-    original_data = np.random.rand(20, 20)
-    np.save(my_test_file, original_data)
+NUM_IMS = 10
+NUM_LAYERS = 21
+NUM_DIMS = 3
 
-    # try to read it back in
-    reader = napari_get_reader(my_test_file)
-    assert callable(reader)
+def test_reader():
+    # get reader function using test zips
+    reader = napari_get_reader(TEST_TILE_PATH)
 
-    # make sure we're delivering the right format
-    layer_data_list = reader(my_test_file)
-    assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
-    layer_data_tuple = layer_data_list[0]
-    assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
+    # reader must be callable
+    assert callable(reader), f"napari_get_reader() did not return a function"
 
-    # make sure it's the same as it started
-    np.testing.assert_allclose(original_data, layer_data_tuple[0])
+    # get result of reader(path)
+    layer_list = reader(TEST_TILE_PATH)
 
+    # resulting layers must be a list
+    assert isinstance(layer_list, list), f"reader does not return list"
+
+    for i in range(len(layer_list)):
+        layer = layer_list[i]
+        # all elements in the layers must be tuples
+        assert isinstance(layer, tuple), f"Layer list element {i} is not tuple"
+
+        # each data in tuples must have correct shape
+        expected_shape = tuple([NUM_IMS] + [im_axis_shape for im_axis_shape in IM_SHAPES[i]])
+        assert layer[0].shape == expected_shape,\
+        f"Layer {i} has shape {layer[0].shape}, expected {expected_shape}"
+
+    # napari must be able to open the path directly
+    v = napari.components.ViewerModel()
+    v.open(TEST_TILE_PATH)
+
+    # viewer must have 21 layers after opening
+    assert len(v.layers) == NUM_LAYERS, f"Expected {NUM_LAYERS} layers after opening in napari viewer, got {len(v.layers)}"
+    # viewer should have 3 dimensions after opening
+    assert v.dims.ndim == NUM_DIMS, f"Expected {NUM_DIMS} dimensions after opening in napari viewer, got {v.dims.ndim}"
 
 def test_get_reader_pass():
+    # path which is not a zip and doesn't contain zips
     reader = napari_get_reader("fake.file")
+    assert reader is None
+
+    # path which is a zip but not a SENTINEL zip
+    reader = napari_get_reader("fake.zip")
+    assert reader is None
+
+    # list of non-sentinel zips
+    reader = napari_get_reader(["fake.zip", "fake2.zip"])
     assert reader is None
